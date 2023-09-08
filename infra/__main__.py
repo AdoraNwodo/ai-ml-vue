6c13ba9e9fff6af060c72b422e515dad82a8a3be
Adora Nwodo <adoranwodo@Adoras-MacBook-Pro.local>
2023-09-08 11:07:36 +0100

    Add css and env variables

---
+++ infra/__main__.py
@@ -0,0 +1,46 @@
+"""A Python Pulumi program"""
+
+import pulumi
+import pulumiverse_vercel as vercel
+
+# Load config settings from `pulumi config set` user commands
+config = pulumi.Config()
+backendDns = config.require("backendDns")
+token = config.require_secret("token")
+repoName = config.require("repoName")
+repoType = config.require("repoType")
+
+provider = vercel.Provider("vercel-provider",
+    api_token = token
+)
+
+project = vercel.Project("vercel-project", 
+    name = "vercel-git-project",
+    framework = "vue",
+    git_repository = vercel.ProjectGitRepositoryArgs(
+        repo = repoName,
+        type = repoType
+    ),
+    opts = pulumi.ResourceOptions(
+        provider = provider
+    )
+)
+
+environment = vercel.ProjectEnvironmentVariable("vercel-env",
+    project_id = project.id,
+    key = "VUE_APP_BACKEND_DNS",
+    value = backendDns,
+    targets = ["production"],
+    opts = pulumi.ResourceOptions(
+        provider = provider
+    )
+)
+
+deployment = vercel.Deployment("vercel-deployment",
+    project_id = project.id,
+    production = True,
+    ref = "main",
+    opts = pulumi.ResourceOptions(
+        provider = provider
+    )
+)
--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+*{
+  background: #191825;
+}
 </style>
